f458e720016eaf03e054eaa159b72d52ac5ab9aa
inninform <157962471+inninform@users.noreply.github.com>
2024-03-17 14:08:12 +0100

    Add files via upload

---
+++ raname.py
@@ -0,0 +1,36 @@
+import random
+
+# male names
+name_male = ["salah","bassim","mohamed","jack","john","alexander","omar","liam","lamine","amine","noah","oliver","james","elijah","abedelah","abdelhak","hamza","henry","lucas","benjamin","theodore","mateo","levi","sebastian","daniel","michael","oussama","sofiane","owen","asher","samuel","ethan","leo","jackson","mason","marwane","zakaria","ahmed","said","ezra","hudson","luca","aiden","joseph","david","jacob","logan","luke","julian","gabriel","grayson","wyatt","matthew","dylan","isaac","elias","anthony","thomas","jayden","carter","santiago","ezekiel","charles","josiah","caleb","cooper","lincoln","miles","hamed","amir","rayan","munir","yassine","yasser","christopher","nathan","isaiah","kai","joshua","andrew","angel","adrian","cameron","nolan","waylon","jaxon","roman","eli","wesley","aaron","ian","christian","ryan","leonardo","brooks","axel","walker","jonathan","easton","everett","weston","bennett","robert","jameson","landon","silas","jose","beau","micah","colton","jordan","jeremiah","parker","greyson","rowan","adam","nicholas","theo","xavier"]
+# female names
+name_female = ["olivia","emma","sara","charlotte","nora","fatiha","amelia","sophia","isabella","ava","mia","evelyn","luna","harper","camila","sofia","scarlett","elizabeth","eleanor","emily","chloe","mila","violet","amina","asia","sokayna","sondoss","penelope","gianna","aria","abigail","ella","avery","hazel","layla","lily","aurora","nova","ellie","madison","grace","isla","willow","zoe","riley","stella","eliana","ivy","victoria","emilia","zoey","naomi","hannah","lucy","elena","lillian","maya","leah","paisley","addison","natalie","valentina","everly","deliah","leilani","madelyn","kinsley","ruby","sophie","alice","genesis","claire","audrey","sadie","aaliyah","joshephine","autumn","brooklyn","quinn","kennedy","cora","savannah","athena","natalia","hailey","aubrey","emery","anna","iris","bella","eloise","skylar","jade","gabriella","ariana","maria","adeline","lydia","sarah","nevaeh","serenity","liliana","ayla","everleigh","raelynn"]
+#family names
+family_name = ["elsher","solace","levine","thatcher","raven","bardot","st.james","hansley","cromwell","ashley","monroe","west","langley","daughtler","madison","marley","ellis","hope","cassidy","lopez","jenkins","poverly","McKenna","gonzales","keller","collymore","stoll","verlice","adler","huxley","ledger","hayes","ford","finnegan","beckett","gatlin","pierce","zimmerman","dawson","wilson","adair","gray","curran","crassus","anderson","adams","carter","hendrix","lennon","gasper","mintz","ashbluff","marblemaw","bozzeli","fellowes","windward","yarrow","yearwood","wixx","humblecut","dustfinger","biddercombe","vespertine","october","gannon","truthbelly","woodgrip","gorestriker","caskcut","oatrun","sagespark","strongblossom","hydrafist","snakeleaf","barlowe","caddel","hart","katz","laurier","madden","elrod","whitlock","ashford","amos","fleet","moses","singh","remington","sharpe","beam","spade","driscoll","undergrove","finch","crawford","cyprus","dagon","lightfoot"]
+
+def random_male_name(): #for give a random male names
+    rnd_ma_nm = random.choice(name_male)
+    return rnd_ma_nm
+def random_female_name(): # for give a random female names
+    rnd_fe_nm = random.choice(name_female)
+    return rnd_fe_nm
+def random_family_name(): # for give a random family names
+    rnd_fm_nm = random.choice(family_name)
+    rnd_fm_nm
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
